ee5be466ef76d621401fc5727374a1dd6a0e041b
Alex Bel <belinua@gmail.com>
2017-12-17 23:53:44 +0200

    updated with delete

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,3 +1,5 @@
+import * as fromAction from './actions';
+
 export const initialState = {
     loaded: false,
     loading: false,
@@ -8,14 +10,25 @@ export function reducer(
     state = initialState,
     action: { type: string, payload: any }
 ) {
-    switch(action.type){
-        case 'ADD_TODO':  {
+    switch (action.type) {
+        case fromAction.ADD_TODO: {
             const todo = action.payload;
             const data = [...state.data, todo];
             return {
                 ...state,
                 data
             }
+        }
+
+        case fromAction.REMOVE_TODO: {
+            console.log(action.payload);
+            const data = state.data.filter(
+                todo => todo.label !== action.payload.label
+            );
+            return {
+                ...state,
+                data,
+            };
         }
     }
 
